a8e527da1100b71feda0d9820377fb8b42da4547
DarshilDhandhiya <122327797+DarshilDhandhiya@users.noreply.github.com>
2023-08-20 21:55:41 +0530

    API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// src/WeatherApp.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './WeatherApp.css';
 
-function App() {
+const API_KEY = '3a025f10f908f6e20e33e01afa73c32b';
+const API_URL = `https://api.openweathermap.org/data/2.5/weather`;
+
+function WeatherApp() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('');
+
+  useEffect(() => {
+    if (city) {
+      axios
+        .get(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
+        .then(response => {
+          console.log(response.data); // Add this line to check the response data
+          setWeatherData(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching weather data:', error);
+        });
+    }
+  }, [city]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="weather-header">Weather App</h1>
+      <div className="input-container">
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={e => setCity(e.target.value)}
+        />
+      </div>
+      {weatherData && (
+        <div className="weather-container">
+          <h2 className="weather-header">
+            Weather in {weatherData.name}, {weatherData.sys.country}
+          </h2>
+          <p className="weather-data">Temperature: {weatherData.main.temp}°C</p>
+          <p className="weather-data">Weather: {weatherData.weather[0].description}</p>
+          <p className="weather-data">Humidity: {weatherData.main.humidity}%</p>
+          <p className="weather-description">
+            Enjoy the {weatherData.weather[0].description} day!
+          </p>
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default WeatherApp;
